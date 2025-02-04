4fff3302f0ac05b5baa10ca854e2baabecf4fb22
Muhamad Refa Fatur Rahman <150717285+refafatur@users.noreply.github.com>
2025-02-04 07:50:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-// emailjs.init("bxrW6kS67mmlitjPy"); // ganti dengan Public Key Anda
+let profileViewCount = localStorage.getItem('profileViewCount') || 0;
+profileViewCount++;
+localStorage.setItem('profileViewCount', profileViewCount);
 
-// document.getElementById("contactForm").addEventListener("submit", function (e) {
-//   e.preventDefault(); // Mencegah reload halaman
-//   const formData = new FormData(e.target);
-//   const name = formData.get("name");
-//   const email = formData.get("email");
-//   const message = formData.get("message");
-//   emailjs
-//     .send("service_4s009oq", "template_m132nie", { //ganti dengan service_id dan template_id
-//       from_name: name,
-//       reply_to: email,
-//       message: message,
-//     })
-//     .then((response) => {
-//       Swal.fire({
-//         icon: "success",
-//         title: "Pesan Terkirim!",
-//         text: "Pesan Anda telah berhasil dikirim. Kami akan segera menghubungi Anda.",
-//       });
-//       e.target.reset(); // reset form setelah pengiriman
-//     })
-//     .catch((error) => {
-//       Swal.fire({
-//         icon: "error",
-//         title: "Gagal Mengirim Pesan",
-//         text: "Silakan coba lagi nanti.",
-//       });
-//       console.error("Error:", error);
-//     });
-// });
+document.getElementById('profile-view-count').textContent = ` ${profileViewCount} Visit`;
