3d196870a3316fd1b14329e23d8604b69f8d09b2
Muhamad Refa Fatur Rahman <150717285+refafatur@users.noreply.github.com>
2025-08-27 00:41:56 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let profileViewCount = localStorage.getItem('profileViewCount') || 0;
-profileViewCount++;
-localStorage.setItem('profileViewCount', profileViewCount);
-
-document.getElementById('profile-view-count').textContent = ` ${profileViewCount} Visit`;
